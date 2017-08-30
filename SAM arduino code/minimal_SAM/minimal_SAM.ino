6b847eca6dc9ed9c12347daf3a62f82d4449f224
Somnitec <arvid@arvidjense.com>
2017-08-30 15:25:43 +0200

    as used at mediamatic

--- SAM arduino code/minimal_SAM/minimal_SAM.ino
+++ SAM arduino code/minimal_SAM/minimal_SAM.ino
@@ -48,7 +48,7 @@ volatile double waterFlow = 0;
 #define blinkBrightness 30
 
 //pumping
-#define liquidAmount 75//ml
+#define liquidAmount 705//ml
 
 //printing
 
--- SAM python code/Printer test.py
+++ SAM python code/Printer test.py
@@ -9,7 +9,7 @@ today = datetime.date.today()
 mylist.append(today)
 
 hDC = win32ui.CreateDC ()
-hDC.CreatePrinterDC ("POS-58")
+hDC.CreatePrinterDC ("Microsoft Print to PDF")
 hDC.StartDoc ("receipt")
 hDC.StartPage ()
 #constructing the page
@@ -47,7 +47,7 @@ fontitalic = win32ui.CreateFont({
 })
 fontbig = win32ui.CreateFont({
     "name": "Roboto",
-    "height": int(fontsize*1.0),
+    "height": int(fontsize*0.9),
     "weight": 700,
 })
 
@@ -56,42 +56,38 @@ hDC.TextOut(170,0*linedistance,"SAM")
 hDC.TextOut(2,1*linedistance,"Symbiotic Autonomous Machine")
 
 hDC.SelectObject(fontitalic)
-hDC.TextOut(0,6*linedistance,"R n."+str(receiptno).zfill(3) )
-hDC.TextOut(230,6*linedistance,time.strftime("%H:%M:%S")+"  "+str(mylist[0]))
+hDC.TextOut(0,3*linedistance,"R n."+str(receiptno).zfill(3) )
+hDC.TextOut(230,3*linedistance,time.strftime("%H:%M:%S")+"  "+str(mylist[0]))
 
 hDC.SelectObject(fontnormal)
-hDC.TextOut(0,8*linedistance,"1 cup kefir soda")
-hDC.TextOut(315,8*linedistance,u"\u20ac"+"%0.2f" %price)
-hDC.TextOut(0,9*linedistance,"BTW 6%")
-hDC.TextOut(315,9*linedistance,u"\u20ac"+"%0.2f" %btw)
-hDC.TextOut(0,11*linedistance,"Total")
-hDC.TextOut(315,11*linedistance,u"\u20ac"+"%0.2f" %total)
+hDC.TextOut(0,5*linedistance,"1 cup kefir soda")
+hDC.TextOut(315,5*linedistance,u"\u20ac"+"%0.2f" %price)
+hDC.TextOut(0,6*linedistance,"BTW 6%")
+hDC.TextOut(315,6*linedistance,u"\u20ac"+"%0.2f" %btw)
+hDC.TextOut(0,8*linedistance,"Total")
+hDC.TextOut(315,8*linedistance,u"\u20ac"+"%0.2f" %total)
 
-hDC.TextOut(50,15*linedistance,"Rate your soda out of 5")
-hDC.TextOut(25,16*linedistance,"on twitter @nonhumanSAM")
+
 
 hDC.SelectObject(fontbig)
-hDC.TextOut(10,18*linedistance,"Thanks for keeping me alive")
-hDC.TextOut(80,19*linedistance,"and functioning!")
+hDC.TextOut(0,10*linedistance,"Thanks for keeping me alive")
+hDC.TextOut(80,11*linedistance,"and functioning!")
 
 hDC.SelectObject(fontitalic)
-hDC.TextOut(90,22*linedistance,"email: sam@nonhuman.club")
-hDC.TextOut(95,int(22.5*linedistance),"web: sam.nonhuman.club")
+hDC.TextOut(90,12*linedistance,"email: sam@nonhuman.club")
+hDC.TextOut(95,int(12.5*linedistance),"web: sam.nonhuman.club")
+
 
 pen = win32ui.CreatePen(0,5, 0)
 hDC.SelectObject(pen)
 hDC.MoveTo(pagewidth,0)#margin line, remove later
 hDC.LineTo(pagewidth,1000)#margin line, remove later
 
-hDC.MoveTo(0,int(3.5*linedistance))
-hDC.LineTo(pagewidth,int(3.5*linedistance))
+hDC.MoveTo(0,int(2.5*linedistance))
+hDC.LineTo(pagewidth,int(2.5*linedistance))
 
-hDC.MoveTo(0,int(14.5*linedistance))
-hDC.LineTo(pagewidth,int(14.5*linedistance))
-
-hDC.MoveTo(0,int(20.5*linedistance))
-hDC.LineTo(pagewidth,int(20.5*linedistance))
-
+hDC.MoveTo(0,int(9.5*linedistance))
+hDC.LineTo(pagewidth,int(9.5*linedistance))
 
 hDC.EndPage ()
 hDC.EndDoc ()
