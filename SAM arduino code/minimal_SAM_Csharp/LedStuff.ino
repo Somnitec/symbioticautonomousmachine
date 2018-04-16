0c73e18295faae5c760922271e38c4633a1e8ac3
SAM.3 <info@arvidjandmarie.com>
2018-04-16 22:00:30 +0200

    reimplemented the changes in arduino sketch, but somehow it won't upload

--- SAM arduino code/minimal_SAM_Csharp/LedStuff.ino
+++ SAM arduino code/minimal_SAM_Csharp/LedStuff.ino
@@ -5,11 +5,11 @@
 //4=cup full, printing note, sends back '0' to ack
 
 
- #define  idle 0
- #define  waitingForPayment 1
- #define  waitingForTapping 2 
- #define  error 3 
- #define  testing 4
+#define  idle 0
+#define  waitingForPayment 1
+#define  waitingForTapping 2
+#define  error 3
+#define  testing 4
 
 
 unsigned long ledTimer = 0;
@@ -28,13 +28,14 @@ void ledStuff() {
     //idle state
     if (stateNow == idle) {
 
-       analogWrite(buttonledpin, fmap(sin(ledPos + (4 * PI) / 3), -1, 1, 100, 255));
+      //analogWrite(buttonledpin, fmap(sin(ledPos + (4 * PI) / 3), -1, 1, 100, 255));
 
     }//waiting for payment state
     else if (stateNow == waitingForPayment) {
 
-      analogWrite(buttonledpin, fmap(sin(ledPos * 4 + (6 * PI) / 3), -1, 1, 50, 255));
-
+      //analogWrite(buttonledpin, fmap(sin(ledPos * 4 + (6 * PI) / 3), -1, 1, 50, 255));
+      buttonled[0] = CHSV(255, 255, fmap(sin(ledPos * 4 + (6 * PI) / 3), -1, 1, 50, 255));
+      FastLED.show();
     }//pumping state
     else if (stateNow == waitingForTapping) {
 
@@ -42,19 +43,28 @@ void ledStuff() {
 
     }//error state
     else if (stateNow == error) {
-      analogWrite(buttonledpin, 255);
+      //analogWrite(buttonledpin, 255);
+      buttonled[0] = CRGB::White;
+      FastLED.show();
       delay(blinkOnTime);
-      analogWrite(buttonledpin, 0);
-       delay(blinkOffTime);
+      //analogWrite(buttonledpin, 0);
+      buttonled[0] = CRGB::Black;
+      FastLED.show();
+      delay(blinkOffTime);
       //ledState = 0;
     }
     //test state
     else if (stateNow == testing) {
-      int pins[] = {led1pin, led2pin, led3pin, buttonledpin};
-      for (int i = 0; i < 4; i++) {
+      int pins[] = {led1pin, led2pin};
+      for (int i = 0; i < 2; i++) {
+        buttonled[0] = CRGB::White;
+        FastLED.show();
         digitalWrite(pins[i], HIGH);
         delay(100);
+        buttonled[0] = CRGB::Black;
+        FastLED.show();
         digitalWrite(pins[i], LOW);
+        delay(100);
       }
     }
   }
@@ -63,7 +73,8 @@ void ledStuff() {
 void breath() {
   analogWrite(led1pin, fmap(sin(ledPos), -1, 1, ledBreathMin, ledBreathMax));
   analogWrite(led2pin, fmap(sin(ledPos + PI / 3), -1, 1, ledBreathMin, ledBreathMax));
-  analogWrite(led3pin, fmap(sin(ledPos + (2 * PI) / 3), -1, 1, ledBreathMin, ledBreathMax));
+  buttonled[0] = CHSV(255, 255, fmap(sin(ledPos + (2 * PI) / 3), -1, 1, ledBreathMin, ledBreathMax));
+  FastLED.show();
 }
 
 void blinkLed(int amount) {
