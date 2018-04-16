8826ce2f3dee336de84174946beea3e0df8e64d0
SAM.3 <info@arvidjandmarie.com>
2018-04-16 15:50:40 +0200

    not sure

---
+++ SAM arduino code/minimal_SAM_Csharp/flowStuff.ino
@@ -0,0 +1,52 @@
+unsigned long flowTimer = 0;
+void flowStuff() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - flowTimer >= flowUpdateTime) {
+    flowTimer = currentMillis;
+    if (nowTapping) {
+      if (waterFlow < tapAmount) {
+        digitalWrite(pump1pin,HIGH);
+        //cmdMessenger.sendCmd(kTapAmount, waterFlow);
+      }else {
+        digitalWrite(pump1pin,LOW);
+        nowTapping = false;
+        cmdMessenger.sendCmd(kTapSucceeded, "all done");
+      }
+    }
+
+    if (nowTappingMilliseconds) {
+      if (tapTimer < tapAmountMilliseconds) {
+        digitalWrite(pump1pin,HIGH);
+        //cmdMessenger.sendCmd(kTapAmount, waterFlow);
+      }else {
+        digitalWrite(pump1pin,LOW);
+        nowTappingMilliseconds = false;
+        cmdMessenger.sendCmd(kTapSucceeded, "all done");
+      }
+    }
+
+
+    /*
+        if (ledState == 3) {
+          digitalWrite(pump1pin, !digitalRead(pump1pin));
+          if (waterFlow < liquidAmount)digitalWrite(pump1pin, HIGH);
+          else {
+            digitalWrite(pump1pin, LOW);
+            ledState = 4;
+            Serial.println("b");
+          }
+
+
+          //Serial.print("waterFlow: ");
+          //Serial.print(waterFlow, 3);
+          //Serial.println(" mL");
+        }
+        else digitalWrite(pump1pin, LOW);
+    */
+  }
+}
+
+void flowSensor()   //measure the quantity of square wave
+{
+  waterFlow += 1.0 / 5.880;
+}
